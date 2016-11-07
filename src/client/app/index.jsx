d9f27ea429d2d7e16c19f56c9f9ccca05ce867c1
Luke Schoen <ltfschoen@gmail.com>
2016-11-07 11:20:46 +1100

    Added LikeComponent

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import LikeComponent from './LikeComponent.jsx';
+
 class App extends React.Component {
     render () {
-        return <p> Hello React!</p>;
+        return (
+            <div>
+                <p>Hello React!</p>
+                <LikeComponent/>
+            </div>
+        );
     }
 }
 
