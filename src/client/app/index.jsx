d0e5b43799cfd5130af3414d12ae804e394b2304
Luke Schoen <ltfschoen@gmail.com>
2016-11-08 09:32:23 +1100

    Added css injection

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -3,6 +3,8 @@ import {render} from 'react-dom';
 
 import LikeComponent from './LikeComponent.jsx';
 
+import '../styles/main.css'
+
 class App extends React.Component {
     render () {
         return (
